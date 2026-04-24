9260bc1fca918900d8f9fbf73cc7ec8c99caf017
di-birt <mardanza@birt.eus>
2026-04-24 10:01:18 +0200

    cambio nombre

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'SegundaAPP',
+  appName: 'DI01_TA01_2627',
   webDir: 'www'
 };
 
